e4a102ca525c491d44de3d5c46aec36123ec7407
Kevin <kevin005005@gmail.com>
2025-10-31 15:17:58 +0100

    Lucky 13 is now fully modularized

--- index.js
+++ index.js
@@ -43,6 +43,68 @@ class Game {
     }
 }
 
+class Result {
+    constructor(game) {
+        this.game = game;
+        this.selected = undefined;
+        this.luckyNumber = undefined;
+        this.setOptions(game.questions.length);
+    }
+
+    setOptions(numberOfQuestions) {
+        // if (numberOfQuestions == 13) {
+        //     this.options = lucky13;
+        // } else {
+        //     this.options = {};
+        // }
+        this.options = lucky13;
+    }
+
+    checkIfInRange(range, number) {
+        let chunks = range.split("-");
+        if (chunks.length == 1) {
+            return number == chunks[0];
+        } else {
+            return chunks[0] <= number && number <= chunks[1];
+        }
+    }
+
+    selectRange(indexOfRangeSelected) {
+        if (isNaN(indexOfRangeSelected) || indexOfRangeSelected < 1 || indexOfRangeSelected > Object.keys(lucky13).length) {
+            return false;
+        }
+        this.selected = indexOfRangeSelected;
+        return true;
+    }
+
+    selectLuckyNumber(luckyNumber) {
+        if (isNaN(luckyNumber) || !this.checkIfInRange(Object.keys(this.options)[this.selected], luckyNumber)) {
+            return false;
+        }
+        this.luckyNumber = luckyNumber;
+        return true;
+    }
+
+    get isBonusWon() {
+        return this.game.numberOfCorrectAnswers == this.luckyNumber;
+    }
+
+    get isRangeCorrect() {
+        return this.checkIfInRange(Object.keys(this.options)[this.selected], this.game.numberOfCorrectAnswers);
+    }
+
+    get prize() {
+        let prize = 0;
+        if (this.isRangeCorrect) {
+            prize += Object.values(this.options)[this.selected];
+        }
+        if (this.isBonusWon) {
+            prize += 25000;
+        }
+        return prize;
+    }
+}
+
 function isAnswerValid(answer) {
     return answer.toLowerCase() == "f" || answer.toLowerCase() == "t"
 }
@@ -54,67 +116,51 @@ for (let index = 0; index < questions.length; index++) {
 }
 
 
-const rl = readline.createInterface({ input: stdin, output: stdout });
-// const numberOfQuestions = 13;
-// // console.log(selectedQuestions)
 
-// function check_if_in_range(range, number) {
-//     let chunks = range.split("-");
-//     if (chunks.length == 1) {
-//         return number == chunks[0];
-//     } else {
-//         return chunks[0] <= number && number <= chunks[1];
-//     }
-// }
+async function demoGame(numberOfQuestions) {
+    const rl = readline.createInterface({ input: stdin, output: stdout });
+    const g = new Game(numberOfQuestions, parsedQuestions);
 
-const g = new Game(13, parsedQuestions);
+    while (!g.isGameOver) {
+        const { question, index: questionIndex } = g.nextQuestion();
+        let answer;
+        do {
+            console.log(question.description);
+            answer = await rl.question("Your answer (T/F): ");
+        } while (!isAnswerValid(answer))
+        g.answerQuestion(questionIndex, answer.toLowerCase() == "t");
+        // console.log(question.correctAnswer, q.userAnswer);
+        console.log("Your answer is", question.isUserAnswerCorrect ? "correct." : "incorrect.");
+    }
+    const r = new Result(g);
+    console.log("Select one of the following ranges:");
 
-while (!g.isGameOver) {
-    const { question, index: questionIndex } = g.nextQuestion();
-    let answer;
+    for (const [index, value] of Object.keys(r.options).entries()) {
+        console.log(index + 1 + ".", value, "-", "$" + lucky13[value].toLocaleString());
+    }
+
+    let indexOfRangeSelected;
     do {
-        console.log(question.description);
-        answer = await rl.question("Your answer (T/F): ");
-    } while (!isAnswerValid(answer))
-    g.answerQuestion(questionIndex, answer.toLowerCase() == "t");
-    // console.log(question.correctAnswer, q.userAnswer);
-    console.log("Your answer is", question.isUserAnswerCorrect ? "correct." : "incorrect.");
+        indexOfRangeSelected = Number(await rl.question(""), 10);
+    } while (!r.selectRange(indexOfRangeSelected - 1));
+
+
+    console.log(r.selected + 1, "selected.")
+
+    let luckyNumber;
+    do {
+        luckyNumber = Number(await rl.question("Select your lucky number: "))
+    } while (!r.selectLuckyNumber(luckyNumber));
+
+    console.log(`Your score is ${g.numberOfCorrectAnswers} out of ${g.questions.length}.`)
+
+    if (r.prize > 0) {
+        console.log(`You won $${r.prize.toLocaleString()}.`);
+    } else {
+        console.log("You lost.");
+    }
+
+    rl.close();
 }
 
-// let indexOfRangeSelected;
-
-// console.log("Select one of the following ranges:");
-
-// for (const [index, value] of Object.keys(lucky13).entries()) {
-//     console.log(index + 1 + ".", value, "-", "$" + lucky13[value].toLocaleString());
-// }
-
-// do {
-//     indexOfRangeSelected = Number(await rl.question(""), 10);
-// } while (isNaN(indexOfRangeSelected) || indexOfRangeSelected < 1 || indexOfRangeSelected > Object.keys(lucky13).length);
-
-
-// const rangeSelected = Object.keys(lucky13)[indexOfRangeSelected - 1];
-// console.log(rangeSelected, "selected.")
-
-// let luckyNumber;
-
-// do {
-//     luckyNumber = Number(await rl.question("Select your lucky number: "))
-// } while (isNaN(luckyNumber) || !check_if_in_range(rangeSelected, luckyNumber));
-
-console.log(`Your score is ${g.numberOfCorrectAnswers} out of ${g.questions.length}.`)
-
-// let bonusWon = numberOfCorrectAnswers == luckyNumber;
-// let prize = lucky13[rangeSelected]
-// if (bonusWon) {
-//     prize += 25000;
-// }
-
-// if (check_if_in_range(rangeSelected, numberOfCorrectAnswers)) {
-//     console.log(`You won $${prize.toLocaleString()}.`);
-// } else {
-//     console.log("You lost.");
-// }
-
-rl.close();
+await demoGame(13);
